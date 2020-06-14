df1cd89e920c745d7a03638a5452b1e6c27f101e
Dat Lam <lqdat2n@gmail.com>
2020-06-14 13:52:12 -0400

    Init lab3

---
+++ labs/lab3/mysiteS20/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Uwindsor study\COMP-8347 - Internet App Distributed Sys\PycharmProjects\labs\lab3\mysiteS20\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
